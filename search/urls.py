47531588bf0f37900fad029ca7e97b87be57d13e
hongju <95hongju@gmail.com>
2018-12-20 14:32:23 -0800

    add module for django.views

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
+app_name = 'search'
 urlpatterns = [
-    path('', views.main, name='main'),
+    url(r'^$', views.main, name='main'),
+    url(r'^update/$', views.update, name='update'),
+    url(r'^searchRS/$', views.searchRS, name='searchRS'),
+    url(r'^file_download/$', views.file_download, name='file_download'),
+    url(r'^upload/$', views.upload, name='upload'),
+    url(r'^upload/result_download/$', views.result_download, name='result_download'),
 ]
