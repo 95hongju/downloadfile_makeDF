ba26cc7de8f02c3010f7c38e82ca46b21eb938b9
hongju <95hongju@gmail.com>
2019-02-07 16:07:42 -0800

    - list edit, remove -fix bug(trying edit, not version showed) - if there are no rsId value, turn in to N/A (default value)

--- blacklist/views.py
+++ blacklist/views.py
@@ -37,9 +37,58 @@ def add_new(request):
         r_rsid = request.POST['rsid']
         r_reason = request.POST['reason']
         r_who = request.POST['who']
+        if(r_rsid ==''):
+            r_rsid='N/A'
         c = curr_ver.blacks_set.create(chr=r_chr,pos=r_pos, rsid=r_rsid, reason=r_reason, who=r_who)
         return HttpResponseRedirect(reverse('blacklist:index'))
     else:
         print(f)
         messages.info(request, 'blank not allowed.. check the input data')
+        return HttpResponseRedirect(reverse('blacklist:index'))
+
+
+def remove(request, id):
+    blk = Blacks.objects.get(pk=id)
+    blk.delete()
+    messages.info(request, 'delete done')
+    return HttpResponseRedirect(reverse('blacklist:index'))
+
+
+def edit(request,id):
+    try:
+        global version
+        blk = Blacks.objects.get(pk=id)
+        q = Version.objects.get(version_name=version)
+        blk_list = q.blacks_set.all()
+        context ={'blk_list':blk_list,'blk':blk,'version':version}
+        return render(request,'blacklist/edit.html',context)
+    except:
+        messages.info(request, 'error : invalid pkID')
+        return HttpResponseRedirect(reverse('blacklist:index'))
+
+
+def edit_done(request,id):
+    try:
+        blk = Blacks.objects.get(pk=id)
+        f=inputForm(request.POST)
+        if f.is_valid():
+            if request.POST['rsid']== '':
+                blk.rsid='N/A'
+            else:
+                blk.rsid = request.POST['rsid']
+            blk.chr = request.POST['chr']
+            blk.pos = request.POST['pos']
+            blk.reason = request.POST['reason']
+            blk.who = request.POST['who']
+
+            blk.save()
+            messages.info(request, 'edit done ! ')
+            return HttpResponseRedirect(reverse('blacklist:index'))
+        else:
+            print(request.POST)
+            print("error")
+            messages.info(request, 'blank not allowed(except rsID)... check the edit data')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    except:
+        messages.info(request, 'error : invalid pkID')
         return HttpResponseRedirect(reverse('blacklist:index'))
